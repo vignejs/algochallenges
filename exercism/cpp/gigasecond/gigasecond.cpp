5e6bf256b92754adf02c27d20184cb307b923bcb
Vignesh Pillay <vignesh.umashankar@gmail.com>
2020-04-14 17:26:13 +0530

    gigasecond

--- exercism/cpp/gigasecond/gigasecond.cpp
+++ exercism/cpp/gigasecond/gigasecond.cpp
@@ -1,5 +1,8 @@
 #include "gigasecond.h"
 
 namespace gigasecond {
+    const ptime advance(boost::posix_time::ptime ptime) {
+        return ptime + seconds(1000000000);
+    }
 
 }  // namespace gigasecond
--- exercism/cpp/gigasecond/gigasecond.h
+++ exercism/cpp/gigasecond/gigasecond.h
@@ -1,7 +1,13 @@
 #if !defined(GIGASECOND_H)
 #define GIGASECOND_H
 
+#include "boost/date_time/posix_time/posix_time.hpp"
+using namespace boost::posix_time;
+
 namespace gigasecond {
+
+    const ptime advance(boost::posix_time::ptime ptime);
+
 
 }  // namespace gigasecond
 
